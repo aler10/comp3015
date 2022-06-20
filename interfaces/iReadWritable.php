09ae9eba880e7dded5c67b7de156c9926578805e
aler10 <78813185+aler10@users.noreply.github.com>
2022-06-20 16:44:19 -0700

    Delete interfaces directory

--- interfaces/iReadWritable.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface IReadWritable {
-    public function getCourses();
-    public function addCourse(Course $course);
-    public function deleteCourse(string $id);
-    public function completeCourse(string $id);
-}
