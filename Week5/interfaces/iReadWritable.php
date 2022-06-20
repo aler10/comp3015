19acc83b8a0c0daed780cf8c9d62177f1fc16cdd
aler10 <78813185+aler10@users.noreply.github.com>
2022-06-20 16:44:55 -0700

    Add files via upload

---
+++ Week5/interfaces/iReadWritable.php
@@ -0,0 +1,8 @@
+<?php
+
+interface IReadWritable {
+    public function getCourses();
+    public function addCourse(Course $course);
+    public function deleteCourse(string $id);
+    public function completeCourse(string $id);
+}
